387352fc86ce93a5ebf6295511a198ebd874134b
ami-a <schrodingerbot@gmail.com>
2020-10-11 05:38:50 +0300

    .

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="TrackEverything",
-    version="1.5",
+    version="1.6.0",
     author="Ami-A",
     author_email="schrodingerbot@gmail.com",
     description="A package that combines detetction, classification and tracking in videos, using AI models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ami-a/TrackEverything",
-    packages=["TrackEverything", 'TrackEverything.examples'],
+    packages=["TrackEverything"],
     #packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
